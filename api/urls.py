e639a095bec02c7737daaeda81d321ee17f04fad
hayunyun <hayunyun@naver.com>
2021-02-03 00:45:10 +0900

    Improve RESTful API

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('user_list/', UserList.as_view(), name="user_list"),
-    path('user_list/<int:user_id>', UserDetail.as_view(), name="user_list"),
+    path('user_list/<int:user_id>', UserDetail.as_view(), name="user_detail"),
+    path('product_list/', ProductList.as_view(), name="product_list"),
+    path('product_list/<int:product_id>', ProductDetail.as_view(), name="product_detail"),
 ]
--- api/views.py
+++ api/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from .serializers import *
 
 # Create your views here.
-
+#### User
 class UserList(APIView):
     def get(self, request):
         model = User.objects.all()
@@ -18,7 +18,6 @@ class UserList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-
 
 
 class UserDetail(APIView):
@@ -48,5 +47,51 @@ class UserDetail(APIView):
         if not self.get_user(user_id):
             return Response(f'User with {user_id} is Not Found in database', status=status.HTTP_404_NOT_FOUND)
         model = self.get_user(user_id)
+        model.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+#### Product
+class ProductList(APIView):
+    def get(self, request):
+        model = Product.objects.all()
+        serializer = ProductSerializer(model, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+class ProductDetail(APIView):
+    def get_product(self, product_id):
+        try:
+            model = Product.objects.get(id=product_id)
+            return model
+        except Product.DoesNotExist:
+            return
+
+    def get(self, request, product_id):
+        if not self.get_product(product_id):
+            return Response(f'User with {product_id} is Not Found in database', status=status.HTTP_404_NOT_FOUND)
+        serializer = ProductSerializer(self.get_product(product_id))
+        return Response(serializer.data)
+
+    def put(self, request, product_id):
+        if not self.get_product(product_id):
+            return Response(f'User with {product_id} is Not Found in database', status=status.HTTP_404_NOT_FOUND)
+        serializer = UserSerializer(self.get_product(product_id), data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+
+    def delete(self, request, product_id):
+        if not self.get_product(product_id):
+            return Response(f'User with {product_id} is Not Found in database', status=status.HTTP_404_NOT_FOUND)
+        model = self.get_product(product_id)
         model.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
